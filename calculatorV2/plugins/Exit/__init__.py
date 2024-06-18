b8457ba59935f2ff2674205e38e9d321e819c8fb
carlosv120 <carlosv.12044@gmail.com>
2024-06-17 23:15:00 -0400

    Adding missing files

---
+++ calculatorV2/plugins/Exit/__init__.py
@@ -0,0 +1,6 @@
+import sys
+from calculatorV2.commands import Command
+
+class ExitCommand(Command):
+    def execute(self):
+        sys.exit("Exiting...")
---
+++ calculatorV2/plugins/Menu/__init__.py
@@ -0,0 +1,9 @@
+from calculatorV2.commands import Command
+
+class MenuCommand(Command):
+    def __init__(self, plugins):
+        self.plugins = plugins
+
+    def execute(self):
+        print("Loaded plugins:", "\t\t".join(self.plugins))
+
